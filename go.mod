a45fb10993655d53cd9f8527f2edd5585f1c6eda
Soner Sayakci <s.sayakci@gmail.com>
2025-04-17 13:54:51 +0200

    Remove Sentry integration and update dependencies in go.mod and go.sum
    - Removed the Sentry initialization code from main.go.
    - Deleted badge.go and shopware.go handler files.
    - Updated go.mod to replace `github.com/getsentry/sentry-go` with `github.com/google/go-cmp` and `golang.org/x/crypto`.
    - Updated go.sum to reflect the changes in dependencies.
    
    This cleanup improves the codebase by removing unused dependencies and handlers.

--- go.mod
+++ go.mod
@@ -3,13 +3,10 @@ module frosh-api
 go 1.15
 
 require (
-	github.com/getsentry/sentry-go v0.29.1
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-github/v53 v53.2.0
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/narqo/go-badge v0.0.0-20190124110329-d9415e4e1e9f
-	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/rs/cors v1.11.1
-	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/oauth2 v0.24.0
 )
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/getsentry/sentry-go"
 	"github.com/julienschmidt/httprouter"
 	"github.com/rs/cors"
 
@@ -14,22 +13,12 @@ import (
 )
 
 func main() {
-	err := sentry.Init(sentry.ClientOptions{
-		Dsn: os.Getenv("SENTRY_URL"),
-	})
-	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
-	}
-
 	router := httprouter.New()
 	router.GET("/v2/github/repositories", handler.ListRepositories)
 	router.GET("/v2/github/contributors", handler.ListContributors)
 	router.GET("/v2/github/issues/:plugin", handler.ListRepositoryIssues)
 	router.GET("/v2/packagist/packages", handler.ListPackages)
-	router.GET("/v2/shopware/sales", handler.ListPluginBuys)
-	router.GET("/v2/shopware/badge/:plugin", handler.GetStoreDownloadBadge)
 	router.POST("/webhook/issue", handler.GithubIssueWebhook)
-	router.GET("/", handler.ListPluginBuys)
 
 	port := os.Getenv("PORT")
 
